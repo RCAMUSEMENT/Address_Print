80c15ce525ddd32bbc5f59bbd742ce7598f30fa4
RCAMUSEMENT <135928308+RCAMUSEMENT@users.noreply.github.com>
2026-03-07 23:08:24 -0700

    Add files via upload

---
+++ AddressPrint.java
@@ -0,0 +1,18 @@
+public class AddressPrint {
+    // This program prints a person's address information to the console
+    // including first name, last name, street address, city, and zip code.
+
+    public static void main(String[] args) {
+        String firstname = "Damien";
+        String lastname = "Andrews";
+        String streetAddress = "8675 Main St";
+        String city = "Random Lake";
+        String zipCode = "53075";
+
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Address_Print
+# AddressPrint
+
+Description:
+
+This Java program prints a person's ('Damien') address information to the console. The information includes the following: first name, last name, street address, city, and zip code.
+All of the aforementioned information is hardcoded as string variables within the programs's main method.
